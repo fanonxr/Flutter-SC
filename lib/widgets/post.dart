88a414d75f542e3cbf34080d4c2a13cd92f2085d
FanonX Rogers <fanonxdevelops@gmail.com>
2020-02-08 14:39:06 -0500

    Set up the indivdual post widget

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,13 +1,214 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:sc_media_flutter/models/user.dart';
+import 'package:sc_media_flutter/pages/home.dart';
+import 'package:sc_media_flutter/widgets/progress.dart';
 
 class Post extends StatefulWidget {
+  // post model
+  final String postId;
+  final String ownerId;
+  final String username;
+  final String location;
+  final String description;
+  final String mediaUrl;
+  final dynamic likes;
+
+  // constructor for the post object
+  Post(
+      {this.postId,
+      this.ownerId,
+      this.username,
+      this.location,
+      this.description,
+      this.mediaUrl,
+      this.likes});
+
+  factory Post.fromDocument(DocumentSnapshot doc) {
+    return Post(
+      postId: doc['postId'],
+      ownerId: doc['owner'],
+      username: doc['username'],
+      location: doc['location'],
+      description: doc['description'],
+      mediaUrl: doc['mediaUrl'],
+      likes: doc['likes'],
+    );
+  }
+
+  // we can now pass methods to the model before we pass it to the widget to display
+  // method to get the like counts
+  int getLikeCount(likes) {
+    // if no likes return 0
+    if (likes == 0) {
+      return 0;
+    }
+    int count = 0;
+    // if the key-value is set to true add a like var
+    likes.values.forEach((val) {
+      if (val == true) {
+        count += 1;
+      }
+    });
+
+    return count;
+  }
+
   @override
-  _PostState createState() => _PostState();
+  _PostState createState() => _PostState(
+      postId: this.postId,
+      ownerId: this.ownerId,
+      username: this.username,
+      location: this.location,
+      description: this.description,
+      mediaUrl: this.mediaUrl,
+      likes: this.likes,
+      likeCount: getLikeCount(this.likes));
 }
 
 class _PostState extends State<Post> {
+  // post model
+  final String postId;
+  final String ownerId;
+  final String username;
+  final String location;
+  final String description;
+  final String mediaUrl;
+  Map likes;
+  int likeCount;
+
+  // constructor for the post object
+  _PostState(
+      {this.postId,
+      this.ownerId,
+      this.username,
+      this.location,
+      this.description,
+      this.mediaUrl,
+      this.likes,
+      this.likeCount});
+
+  // method to build a post header widget
+  buildPostHeader() {
+    return FutureBuilder(
+      future: usersRef.document(ownerId).get(),
+      builder: (context, snapshot) {
+        // display loading if there is no data
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+
+        // once data recieved load user data into the pose
+        User user = User.fromDocument(snapshot.data);
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+            backgroundColor: Colors.grey,
+          ),
+          title: GestureDetector(
+            onTap: () => print("showing profile"),
+            child: Text(
+              user.username,
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+          ),
+          subtitle: Text(location),
+          trailing: IconButton(
+            onPressed: () => print("deleteing post"),
+            icon: Icon(Icons.more_vert),
+          ),
+        );
+      },
+    );
+  }
+
+  // method to build a post image widget
+  buildPostImage() {
+    return GestureDetector(
+      onDoubleTap: () => print("liking post"),
+      child: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Image.network(mediaUrl),
+        ],
+      ),
+    );
+  }
+
+  // method to build a post footage
+  buildPostFootage() {
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 40.0, left: 20.0),
+            ),
+            GestureDetector(
+              onTap: () => print("Liking post"),
+              child: Icon(
+                Icons.favorite_border,
+                size: 28.0,
+                color: Colors.pink,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(right: 20.0),
+            ),
+            GestureDetector(
+              onTap: () => print("showing comments"),
+              child: Icon(
+                Icons.chat,
+                size: 28.0,
+                color: Colors.blue[900],
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "$likeCount likes",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            )
+          ],
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 20.0),
+              child: Text(
+                "$username ",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Expanded(
+              child: Text(description),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Post");
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        // buildPostHeader(),
+        buildPostImage(),
+        buildPostFootage()
+      ],
+    );
   }
 }
